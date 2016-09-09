84efffdc23dcf0bdf4a8f6ecaf7cd7dc83f74f1a
Alfredo Perez <perezalfredo14@gmail.com>
2016-09-08 19:40:07 -0700

    Trying using smtp gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,14 +85,21 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :user_name            => ENV['GMAIL_USERNAME'],
+     :password             => ENV['GMAIL_PASSWORD'],
+     :authentication       => "plain",
+     :enable_starttls_auto => true
+    }
   }
+  # :address        => 'smtp.sendgrid.net',
+  # :port           => '587',
+  # :authentication => :plain,
+  # :user_name      => ENV['SENDGRID_USERNAME'],
+  # :password       => ENV['SENDGRID_PASSWORD'],
+  # :domain         => 'heroku.com',
+  # :enable_starttls_auto => true
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
