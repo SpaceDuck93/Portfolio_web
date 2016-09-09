c9e4f27dc3027c44c47879f9cb2e256e2c6127bd
Alfredo Perez <perezalfredo14@gmail.com>
2016-09-08 18:30:22 -0700

    removing environments

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+api_key: 'key-8723013311f83250ffd232a3f80291cd'
+domain: 'alfredodev.com'
+username: 'postmaster@alfredodev.com'
+password: '77640bb05a84978a7aaf3043cd516ecf'
+gmail_username: 'perezalfredo14@gmail.com'
+gmail_password: 'Apalvarez1093..'
